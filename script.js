390ec8dee3c0cc404f48afdf150b488274c4af47
Mariia Butolina <104693230+butolinka@users.noreply.github.com>
2022-06-07 18:22:23 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,152 @@
+gsap.from('.myself',{y:-200, ease:"bounce", duration:2})
+
+gsap.to('.textTransition',{ text: "&lt;B&gt;<b>utolina <br>I am a WE</b> &lt;/B&gt;", duration:3})
+
+const back = document.querySelector('#back');
+const next = document.querySelector('#next');
+
+const photos =['html.png', 'bootstrap.png', 'gsapp.png'];
+let i=0;
+next.addEventListener('click', ()=>{
+    i++;
+    if(i>photos.length-1){
+        i=0;
+    }
+    document.querySelector('#pictures').src=photos[i];
+})
+back.addEventListener('click', ()=>{
+    i--;
+    if(i<0){
+        i=photos.length-1;
+    }
+    document.querySelector('#pictures').src=photos[i];
+})
+
+particlesJS("particles-js", {
+  "particles": {
+    "number": {
+      "value": 77,
+      "density": {
+        "enable": false,
+        "value_area": 3551.164387345227
+      }
+    },
+    "color": {
+      "value": "#ffffff"
+    },
+    "shape": {
+      "type": "circle",
+      "stroke": {
+        "width": 0,
+        "color": "#000000"
+      },
+      "polygon": {
+        "nb_sides": 12
+      },
+      "image": {
+        "src": "img/github.svg",
+        "width": 100,
+        "height": 100
+      }
+    },
+    "opacity": {
+      "value": 0.4489553770423464,
+      "random": true,
+      "anim": {
+        "enable": false,
+        "speed": 1,
+        "opacity_min": 0.1,
+        "sync": false
+      }
+    },
+    "size": {
+      "value": 51.29459670609772,
+      "random": true,
+      "anim": {
+        "enable": false,
+        "speed": 40,
+        "size_min": 0.1,
+        "sync": false
+      }
+    },
+    "line_linked": {
+      "enable": true,
+      "distance": 0,
+      "color": "#ffffff",
+      "opacity": 0.4,
+      "width": 1
+    },
+    "move": {
+      "enable": true,
+      "speed": 3,
+      "direction": "none",
+      "random": true,
+      "straight": false,
+      "out_mode": "out",
+      "bounce": false,
+      "attract": {
+        "enable": false,
+        "rotateX": 600,
+        "rotateY": 1200
+      }
+    }
+  },
+  "interactivity": {
+    "detect_on": "canvas",
+    "events": {
+      "onhover": {
+        "enable": false,
+        "mode": "grab"
+      },
+      "onclick": {
+        "enable": true,
+        "mode": "push"
+      },
+      "resize": true
+    },
+    "modes": {
+      "grab": {
+        "distance": 400,
+        "line_linked": {
+          "opacity": 1
+        }
+      },
+      "bubble": {
+        "distance": 400,
+        "size": 80,
+        "duration": 2,
+        "opacity": 8,
+        "speed": 3
+      },
+      "repulse": {
+        "distance": 400,
+        "duration": 0.4
+      },
+      "push": {
+        "particles_nb": 4
+      },
+      "remove": {
+        "particles_nb": 2
+      }
+    }
+  },
+  "retina_detect": true
+});
+var count_particles, stats, update;
+stats = new Stats;
+stats.setMode(0);
+stats.domElement.style.position = 'absolute';
+stats.domElement.style.left = '0px';
+stats.domElement.style.top = '0px';
+document.body.appendChild(stats.domElement);
+count_particles = document.querySelector('.js-count-particles');
+update = function() {
+  stats.begin();
+  stats.end();
+  if (window.pJSDom[0].pJS.particles && window.pJSDom[0].pJS.particles.array) {
+    count_particles.innerText = window.pJSDom[0].pJS.particles.array.length;
+  }
+  requestAnimationFrame(update);
+};
+requestAnimationFrame(update);;
+
